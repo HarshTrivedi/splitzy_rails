3f05dcfc4667c9c8c483af69a80345608f27cfd4
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-05-22 18:54:23 +0530

    Implement basic choose_method for language

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,3 +1,13 @@
 class Language < ActiveRecord::Base
   has_many :words
+
+
+  def choose_word( user = nil)
+  	## To be much improved !!
+  	all_words = self.words
+  	words_left = all_words.select{|x| x.syllabifications.size == 0}
+  	return words_left.first , (all_words.size - words_left.size ) , all_words.size
+  end
+
+
 end

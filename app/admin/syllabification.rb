e8a61e982f5c1bbf78b7b101d759f53061f1f09e
Prelang Builder <builder@prelang.com>
2015-05-22 06:10:41 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Syllabification' to allow for create, update

--- app/admin/syllabification.rb
+++ app/admin/syllabification.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Syllabification do
+  permit_params :value, :note
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters

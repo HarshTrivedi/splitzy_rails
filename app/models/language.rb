64e0eb4c74c3dde5ed8b3dfa08be051cec8d346f
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-05-23 19:04:30 +0530

    attempt to make queries little faster.

--- app/models/language.rb
+++ app/models/language.rb
@@ -3,13 +3,14 @@ class Language < ActiveRecord::Base
 
 
   def choose_word( user = nil , skipped_list = []) 
-  	## To be much improved !!
-  	skipped_list = skipped_list || ""
-  	skipped_list = skipped_list.split(",")
-  	all_words = self.words
-  	words_left = all_words.select{|x| x.syllabifications.size == 0} - skipped_list.map{|x| Word.find_by_id(x)}
-  	words_left = words_left.sort_by(&:value)
-  	return words_left.first , (all_words.size - words_left.size ) , all_words.size
+    skipped_list = skipped_list || ""
+    skipped_list = skipped_list.split(",").map(&:to_i)
+    all_word_ids = self.words.where(:syllabifications_count => 0).select(:id).map(&:id)
+    word_ids_left = all_word_ids - skipped_list   
+    skipped_list.each{|x| all_word_ids.delete(x) }
+    words_left = Word.where( :id => word_ids_left ).sort_by(&:value)
+    all_words_size = all_word_ids.size
+    return words_left.first , ( all_words_size - words_left.size ) , all_words_size
   end
 
 

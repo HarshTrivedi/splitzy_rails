eb5d5bb729a642b41ebfda258a5dda3dba50cd66
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-05-23 20:46:40 +0530

    Use psql ordering for sorting words instead of ruby ordering. Change implementation a little to make it faster

--- app/models/language.rb
+++ app/models/language.rb
@@ -8,7 +8,7 @@ class Language < ActiveRecord::Base
     all_word_ids = self.words.where(:syllabifications_count => 0).select(:id).map(&:id)
     word_ids_left = all_word_ids - skipped_list   
     skipped_list.each{|x| all_word_ids.delete(x) }
-    words_left = Word.where( :id => word_ids_left ).sort_by(&:value)
+    words_left = Word.where( :id => word_ids_left ).order(:value).limit(1)
     all_words_size = all_word_ids.size
     return words_left.first , ( all_words_size - words_left.size ) , all_words_size
   end

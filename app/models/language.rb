ef77fc6e83124e4392acccda10a8afc89dcf7ba0
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-05-24 07:55:08 +0530

    Add little randomness so that each user in queue gets unique words for making syllabification (atleast high probability of unique words coming)

--- app/models/language.rb
+++ app/models/language.rb
@@ -9,11 +9,11 @@ class Language < ActiveRecord::Base
     non_syllabified_word_ids = self.words.where(:syllabifications_count => 0).select(:id).map(&:id)
     word_ids_left = non_syllabified_word_ids - skipped_list   
     skipped_list.each{|x| non_syllabified_word_ids.delete(x) }
-    words_left = Word.where( :id => word_ids_left ).order(:value).limit(1)
+    words_left = Word.where( :id => word_ids_left ).order(:value).limit(10)
     non_syllabified_words_size = non_syllabified_word_ids.size
     
     all_words_size = Word.count
-    return words_left.first , ( all_words_size - non_syllabified_words_size ) , all_words_size
+    return words_left.sample , ( all_words_size - non_syllabified_words_size ) , all_words_size
 
   end
 

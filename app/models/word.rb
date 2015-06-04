d05fb33ecf71d791db64e36fcb47737194509dd1
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-06-04 14:22:55 +0530

    fix few bugs in scopes

--- app/models/word.rb
+++ app/models/word.rb
@@ -10,9 +10,19 @@ class Word < ActiveRecord::Base
   scope :three_plus_syllabified , ->{  Word.where( "syllabifications_count > ?" , 3) }
   scope :syllabified ,         ->{  Word.where.not(:syllabifications_count =>  0) }  
   scope :marked ,         ->{  Word.where(:marked =>  true) }
-  scope :potentially_wrong , ->{ Word.where.not(:syllabifications_count => 0).select{ |word|  ( [word.syllabifications.first.value.strip ] + (word.suggestion || "").split("&").map(&:strip)  ).uniq.size != 1  } }
+  scope :potentially_wrong , ->{ 
+    syllabified_words = Word.where.not(:syllabifications_count => 0)
+    potentially_wrong_words = syllabified_words.select{ |word|  ( [word.syllabifications.first.value.strip ] + (word.suggestion || "").split("&").map(&:strip)  ).uniq.size != 1  } 
+    potentially_wrong_word_ids = potentially_wrong_words.map(&:id)
+    Word.where( :word_id => potentially_wrong_word_ids )
+  }
 
-  scope :unresolved_multi_syllabified , ->{ Word.where.not(:syllabifications_count => 0).select{|word| word.syllabifications.map(&:value).map(&:strip).uniq.size != 1 } }
+  scope :unresolved_multi_syllabified , ->{  
+      syllabified_words = Word.where.not(:syllabifications_count => 0)
+      unresolved_words = syllabified_words.select{|word| word.syllabifications.map(&:value).map(&:strip).uniq.size != 1 }
+      unresolved_word_ids = unresolved_words.map(&:id)
+      Word.where( :word_id => unresolved_word_ids )
+  }
 
 
   validates :language, :presence => true
